d5c5a8c4d2bb4e62b5f33f33c1e4ff3022e53678
kswonder <onlylovex5@gmail.com>
2023-11-01 13:22:56 +0900

    gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,12 +4,12 @@ RULE = "Find the greatest common divisor of given numbers."
 
 
 def get_correct_answer():
-    a = random.randrange(0, 31, 2)
-    b = random.randrange(30, 101, 10)
-    question = f'{a} {b}'
-    while a != b:
-        if a > b:
-            a = a - b
+    num_1 = random.randrange(0, 31, 2)
+    num_2 = random.randrange(30, 101, 10)
+    question = f'{num_1} {num_2}'
+    while num_1 != num_2:
+        if num_1 > num_2:
+            num_1 = num_1 - num_2
         else:
-            b = b - a
-    return str(question), str(b)
+            num_2 = num_2 - num_1
+    return str(question), str(num_2)
